d33aee09fc606d5a59b873a411968a31b731b41b
donchelli <tim.melcherson@gmail.com>
2020-10-08 00:14:08 +0200

    Vote result pie arc angle is dynamic. Trying to change value with animation

--- lib/screens/vote_result_page/vote_result_widget.dart
+++ lib/screens/vote_result_page/vote_result_widget.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:swiftvote/themes/themes.dart';
-import 'dart:math';
 
 class VoteResultWidget extends StatefulWidget {
   @override
   State createState() => _VoteResultWidgetState();
 }
 
-class _VoteResultWidgetState extends State<VoteResultWidget> {
+class _VoteResultWidgetState extends State<VoteResultWidget> with SingleTickerProviderStateMixin  {
+  double _arcSecondRad = 1;
+  AnimationController _animationController;
+  Duration _arcAnimationDuration = Duration(milliseconds: 500);
+  Animation<double> _arcAnimation;
+
+
+  @override
+  void initState() {
+    _animationController =  AnimationController(vsync: this, duration: _arcAnimationDuration);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  // void _arcAnimation() {
+  //   _animationController.drive(
+  //     Tween<double>(
+  //       begin: _arcSecondRad,
+  //       end: _arcSecondRad += 0.2,
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,25 +45,53 @@ class _VoteResultWidgetState extends State<VoteResultWidget> {
             DecorationThemes.cardBoxShadow,
           ],
         ),
-        child: Center(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: 350,
-                width: 350,
-                child: CustomPaint(
-                  painter: CirclePainter(),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: MediaQuery.of(context).size.width * 0.6,
+                  child: CustomPaint(
+                    painter: CirclePainter(),
+                  ),
                 ),
-              ),
-              Container(
-                height: 350,
-                width: 350,
-                child: CustomPaint(
-                  painter: ArcPainter(),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: MediaQuery.of(context).size.width * 0.6,
+                  child: CustomPaint(
+                    painter: ArcPainter(this._arcSecondRad),
+
+                    // painter: ArcPainter(_animationController, _arcSecondRad),
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+            FlatButton(
+              color: Colors.blue[200],
+              onPressed: () {
+                // _arcAnimation = Tween(begin: _arcSecondRad, end: _arcSecondRad + 0.5).animate(_animationController)
+                //   ..addListener(() {
+                //     setState(() {
+                //       _arcSecondRad = _arcAnimation.value;
+                //     });
+                //   });
+                setState(() {
+                  _arcSecondRad += 0.1;
+                });
+              },
+              child: Text('+'),
+            ),FlatButton(
+              color: Colors.red[300],
+              onPressed: () {
+
+                setState(() {
+                  _arcSecondRad -= 0.1;
+                });
+              },
+              child: Text('--'),
+            ),
+          ],
         ),
       ),
     );
@@ -61,6 +114,15 @@ class CirclePainter extends CustomPainter {
 }
 
 class ArcPainter extends CustomPainter {
+
+  // final AnimationController controller;
+  // final Animation<double> animation;
+  final double arcSecondRad;
+
+  ArcPainter(this.arcSecondRad);
+
+
+
   @override
   void paint(Canvas canvas, Size size) {
     print('SIZE OF ArcPainter: $size');
@@ -72,7 +134,7 @@ class ArcPainter extends CustomPainter {
       Rect.fromCenter(
           center: Offset(size.height / 2, size.width / 2), height: size.height, width: size.width),
       0,
-      2.14,
+      arcSecondRad,
       true,
       paint1,
     );
